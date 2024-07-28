79560e615e7993e4a4fdde2fd0d026cf7b786020
oliver-gao <oliver.gao@rea-group.com>
2024-07-29 09:48:00 +1000

    Add custom file type support

--- nvim/init.lua
+++ nvim/init.lua
@@ -195,6 +195,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- load custom keymaps
 require 'custom.keymaps'
+require 'custom.filetypes'
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
