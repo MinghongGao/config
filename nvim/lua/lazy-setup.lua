0f532a642e69e1a65800ee6060634782ebdca8b0
oliver-gao <oliver.gao@rea-group.com>
2024-08-15 16:47:03 +1000

    fix which-key warnings

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -2,14 +2,20 @@ local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = 'plugins'
 local opts = {
+  install = {
+    colorscheme = {
+      'catppuccin',
+    },
+  },
+  change_detection = {
+    notify = false,
+  },
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
