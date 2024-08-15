fe7957672ae82a1cc712496dc04ee313166ca63b
oliver-gao <oliver.gao@rea-group.com>
2024-08-15 15:04:53 +1000

    combine keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@ require 'core.globals'
 require 'core.options'
 require 'core.keymaps'
 require 'core.commands'
-require 'custom.keymaps'
 require 'custom.filetypes'
 
 require 'lazy-setup'
--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -29,3 +29,14 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- custom keymap
+
+-- opens lazygit in a tmux popup
+vim.keymap.set('n', '<leader>gg', '<cmd>!tmux popup -E -h 80\\% -w 80\\% -d ' .. vim.fn.getcwd() .. ' -- lazygit <CR><CR>', { desc = 'Go LazyGit' })
+vim.keymap.set('n', '<leader>gt', '<cmd>!tmux popup -E -h 80\\% -w 80\\% -d ' .. vim.fn.getcwd() .. ' -- <CR><CR> ', { desc = 'Go to Terminal' })
+vim.keymap.set('n', '<leader>md', '<cmd>Glow<CR>', { desc = 'Toggle Markdown' })
+vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<CR>', { desc = 'Toggle Git blame' })
+vim.keymap.set('n', '<leader>gp', '<cmd>GpChatNew popup<CR>', { desc = 'Chatgpt new popup' })
+
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = 'Toggle diagnostic float' })
