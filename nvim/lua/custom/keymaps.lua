70116ba7216f6afa384950b0a2a9a0b5333e104b
oliver-gao <oliver.gao@rea-group.com>
2024-08-15 11:49:38 +1000

    remove dadbod ui

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -4,7 +4,6 @@
 vim.keymap.set('n', '<leader>gg', '<cmd>!tmux popup -E -h 80\\% -w 80\\% -d ' .. vim.fn.getcwd() .. ' -- lazygit <CR><CR>', { desc = 'Go LazyGit' })
 vim.keymap.set('n', '<leader>gt', '<cmd>!tmux popup -E -h 80\\% -w 80\\% -d ' .. vim.fn.getcwd() .. ' -- <CR><CR> ', { desc = 'Go to Terminal' })
 vim.keymap.set('n', '<leader>md', '<cmd>Glow<CR>', { desc = 'Toggle Markdown' })
-vim.keymap.set('n', '<leader>db', '<cmd>DBUIToggle<CR>', { desc = 'Toggle DB UI' })
 vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<CR>', { desc = 'Toggle Git blame' })
 vim.keymap.set('n', '<leader>gp', '<cmd>GpChatNew popup<CR>', { desc = 'Chatgpt new popup' })
 
