4ccc7a4f622ccb47ce91ef2410432ff5e8e87bbb
oliver-gao <oliver.gao@rea-group.com>
2024-08-16 14:39:51 +1000

    replace neotree with nvim tree

--- nvim/lua/oliver/core/globals.lua
+++ nvim/lua/oliver/core/globals.lua
@@ -1,9 +1,6 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- Disable netrw as file explorer, will use neo-tree instead through kickstart plugins.
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
